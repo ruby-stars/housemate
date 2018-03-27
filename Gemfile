666b80427f112ba6cdba0143081a1a651213c9b5
Eva Stolz <evasmail@gmail.com>
2018-03-27 23:03:26 +0200

    Added MiniMagick to Gemfile and creating resized image version on upload

--- Gemfile
+++ Gemfile
@@ -16,6 +16,7 @@ gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
 gem 'sprockets-rails'
 gem 'carrierwave', '~> 1.2.2'
+gem 'mini_magick'
 
 group :development, :test do
   gem 'pry-rails'
--- app/uploaders/house_image_uploader.rb
+++ app/uploaders/house_image_uploader.rb
@@ -1,7 +1,7 @@
 class HouseImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -19,6 +19,7 @@ class HouseImageUploader < CarrierWave::Uploader::Base
   end
 
   # Process files as they are uploaded:
+  process resize_to_fit: [400, 400]
   # process scale: [200, 300]
   #
   # def scale(width, height)
@@ -26,15 +27,19 @@ class HouseImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process resize_to_fit: [50, 50]
-  # end
+  version :thumb do
+    process resize_to_fill: [200, 200]
+  end
+
+  version :small_thumb, from_version: :thumb do
+    process resize_to_fill: [20, 20]
+  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_whitelist
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_whitelist
+    %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
