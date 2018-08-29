8004161725bb06cea1a2da6ca3873eaae1a268ec
Julia Wolf <juliaingeburgwolf@gmail.com>
2018-08-29 22:23:03 +0200

    quick fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,18 +9,15 @@ class User < ApplicationRecord
   has_many :mates
 
   def house_mate?
-<<<<<<< HEAD
     mates.any?
   end
 
   def house_manager?
     mates.any? { |mate| mate.house_manager? }
-=======
-  mates.any?
+    mates.any?
   end
 
   def house_manager?
   mates.any? { |mate| mate.house_manager? }
->>>>>>> 90d4444aeea8b53ff846541249952bbe2718ae39
   end
 end
