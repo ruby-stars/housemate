7dcca4a953ffb9e532d78758eef4509e336b3a1a
Eva Stolz <evasmail@gmail.com>
2018-09-04 08:47:35 +0200

    fixed houses#destroy fk error

--- app/models/house.rb
+++ app/models/house.rb
@@ -1,6 +1,6 @@
 class House < ApplicationRecord
   has_many :groups, dependent: :destroy
-  has_many :mates
+  has_many :mates, dependent: :delete_all
   has_many :users, through: :mates
 
   mount_uploader :HouseImage, HouseImageUploader
