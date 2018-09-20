556f1313243498a4cce04737efc4180e140d8165
Eva Stolz <evasmail@gmail.com>
2018-09-20 16:55:01 +0200

    saving additional user info on signup
    added fields to registration view and parameter sanitization in App controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,19 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   protect_from_forgery with: :exception
+
   rescue_from CanCan::AccessDenied do |exception|
     respond_to do |format|
     	format.json { head :forbidden}
     	format.html { redirect_to houses_url, alert: "You're not authorized to perform this action."}
     end
 	end
+
+	protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :firstname, :lastname, :bio, :birthday, :avatar])
+  end
+
 end
