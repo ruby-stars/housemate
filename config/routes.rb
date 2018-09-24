699412a48b0b148e25135c8a1070454f9f8f94b6
Eva Stolz <evasmail@gmail.com>
2018-09-24 18:53:01 +0200

    changed path after user profile update

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations' }
+  
   resources :houses do
     resources :groups do
       resources :tasks
