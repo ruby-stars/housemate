c3ff457c5c8a784ce42d990490691351816aef59
Eva Stolz <evasmail@gmail.com>
2018-09-25 16:51:08 +0200

    default user image (stage 1 - not working yet)

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -14,12 +14,10 @@ class AvatarUploader < CarrierWave::Uploader::Base
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url(*args)
-  #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  #
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
+  def default_url(*args)
+    #{}"/images/default/default-avatar.png"
+    ActionController::Base.helpers.asset_path("default/" + [version_name, "default-avatar.png"].compact.join('_'))
+  end
 
   # Process files as they are uploaded:
   process resize_to_fit: [400, 400]
