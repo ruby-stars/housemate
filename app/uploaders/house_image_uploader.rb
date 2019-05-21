511c8de19b1599ef64755715fec6d3ac6866b587
Feña Agar <fernando.agar@gmail.com>
2019-05-21 19:42:11 +0200

    Added custom image path so you can use two different buckets (#81)

--- app/uploaders/house_image_uploader.rb
+++ app/uploaders/house_image_uploader.rb
@@ -13,7 +13,8 @@ class HouseImageUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    image_path = ENV["IMAGE_PATH"] || "default"
+    "uploads/#{model.class.to_s.underscore}/#{image_path}/#{mounted_as}/#{model.id}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
