fe01d5a6ee4817e7854c56e4d17bce9f87daf061
Feña Agar <fernando.agar@gmail.com>
2019-05-21 20:44:52 +0200

    Adjusted schema, changed default no-reply email address, seeds for houses and users (#85)

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+4.times do |i|
+  puts "Creating House/User #{i}"
+  u = User.create(email: "user_#{i}@example.com", password: "1234567890")
+  h = House.create(name: "House #{i}")
+  Mate.create(user_id: u.id, house_id: h.id, house_manager: true)
+end
