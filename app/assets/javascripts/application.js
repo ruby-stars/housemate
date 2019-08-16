f12f748069894e24daf28ba82e86e8b4f7225de0
Eva Stolz <evasmail@gmail.com>
2019-08-16 15:33:45 +0200

    Add WOWjs animations

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,6 +21,8 @@
 
 $(document).on('turbolinks:load', function () {
 
+  // Initiate the wowjs animation library
+  new WOW().init();
 
   // Header scroll class
   $(window).scroll(function() {
@@ -63,45 +65,5 @@ $(document).on('turbolinks:load', function () {
   //     $('#preloader').delay(100).fadeOut('slow', function () {
   //       $(this).remove();
   //     });
-  //   }
-  // });
-
-
-  
- 
-
-
-  // // Smooth scroll for the menu and links with .scrollto classes
-  // $('.nav-menu a, #mobile-nav a, .scrollto').on('click', function() {
-  //   console.log("smooth scroll fired")
-  //   if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-  //     var target = $(this.hash);
-  //     if (target.length) {
-  //       var top_space = 0;
-
-  //       if ($('#header').length) {
-  //         top_space = $('#header').outerHeight();
-
-  //         if (! $('#header').hasClass('header-scrolled')) {
-  //           top_space = top_space - 20;
-  //         }
-  //       }
-
-  //       $('html, body').animate({
-  //         scrollTop: target.offset().top - top_space
-  //       }, 1500, 'easeInOutExpo');
-
-  //       if ($(this).parents('.nav-menu').length) {
-  //         $('.nav-menu .menu-active').removeClass('menu-active');
-  //         $(this).closest('li').addClass('menu-active');
-  //       }
-
-  //       if ($('body').hasClass('mobile-nav-active')) {
-  //         $('body').removeClass('mobile-nav-active');
-  //         $('#mobile-nav-toggle i').toggleClass('fa-times fa-bars');
-  //         $('#mobile-body-overly').fadeOut();
-  //       }
-  //       return false;
-  //     }
   //   }
   // });
